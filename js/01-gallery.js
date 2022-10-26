5211bd749417d7a1f116c3e05745004cb8e3fd68
FrancuzAlexandr <francuz_s@ukr.net>
2022-10-26 21:41:39 +0300

    hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+  .map(({preview, original, description}) => {
+    return `
+    <div class="gallery__item">
+     <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+      </a>
+     </div>
+`;
+  })
+  .join("");
+}
+
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainer.addEventListener("click", clickImage);
+
+function clickImage (event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+instance.show();
+
+galleryContainer.addEventListener('keydown', event => {
+    if(event.code === 'Escape') {
+        instance.close();
+    }
+});
+}
+
+
+
+
